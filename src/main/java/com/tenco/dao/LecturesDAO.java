e61e8d356e4b146383bdf7d65af74ef70ee8d822
수현 <i.suhyun1024@gmail.com>
2026-09-14 16:37:05 +0900

    강의명 전체로 검색하는 메서드 추가

--- src/main/java/com/tenco/dao/LecturesDAO.java
+++ src/main/java/com/tenco/dao/LecturesDAO.java
@@ -189,6 +189,27 @@ public class LecturesDAO {
         return null;
     }
 
+    // 강의명(전체)로 단건조회 (내부에서만 사용)
+    // !!! 완전히 동일한 강의명이 있다면 먼저 저장된 값이 나옴
+    public Lectures getLectureByFullname(String name) {
+        String sql = "SELECT * FROM lectures WHERE lecture_name = ?";
+
+        try (Connection connect = DatabaseUtil.getConnection()) {
+            try (PreparedStatement pstmt = connect.prepareStatement(sql)) {
+                pstmt.setString(1, name);
+
+                try (ResultSet rs = pstmt.executeQuery()) {
+                    if (rs.next()) {
+                        return createLectures(rs);
+                    }
+                }
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return null;
+    }
+
     // 메서드 추출
     private static Lectures createLectures(ResultSet rs) throws SQLException {
         Lectures lectures = new Lectures();
